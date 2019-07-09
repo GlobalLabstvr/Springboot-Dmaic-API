984fd9e4ffa6a4e645ccef4ba7c1896c9529e293
Kathambaritvr <sujeesatheeshveera@gmail.com>
2019-07-09 10:34:01 +0530

    dmaic service works

--- dmaic/src/main/java/com/glabs/dmaic/dmaic_app/DmaicRepository.java
+++ dmaic/src/main/java/com/glabs/dmaic/dmaic_app/DmaicRepository.java
@@ -1,6 +1,6 @@
 package com.glabs.dmaic.dmaic_app;
 
-//import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DmaicRepository extends JpaRepository<Dmaic, Long> 
 {
-	//List<Dmaic> findByUserId(Long userId);
+	
 }
